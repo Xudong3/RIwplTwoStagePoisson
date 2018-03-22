eed35f9e6bdcc755694be77927b3dd4a1d1ca9c3
Xudong Huang <xudonghuang3@hotmail.com>
2018-03-22 18:23:39 +1300

    fast

--- RIwplTwoStagePoisson_fast.R
+++ RIwplTwoStagePoisson_fast.R
@@ -869,8 +869,34 @@ abline(h=0)
 
 #create a table for latex
 #install.packages("xtable")
-#library(xtable)
-print(xtable(table_fit))
+library(xtable)
+construct_header <- function(df, grp_names, span, align = "c", draw_line = T) {
+   if (length(align) == 1) align <- rep(align, length(grp_names))
+   if (!all.equal(length(grp_names), length(span), length(align)))
+      stop("grp_names and span have to have the same length!")
+   
+   if (ncol(df) < sum(span)) stop("Span has to be less or equal to the number of columns of df") 
+   
+   header <- mapply(function(s, a, grp) sprintf("\\multicolumn{%i}{%s}{%s}", s, a, grp),
+                    span, align, grp_names)
+   header <- paste(header, collapse = " & ")
+   header <- paste0(header, " \\\\")
+   
+   if (draw_line) {
+      # where do we span the lines:
+      min_vals <- c(1, 1 + cumsum(span)[1:(length(span) - 1)])
+      max_vals <- cumsum(span)
+      line <- ifelse(grp_names == "", "", 
+                     sprintf("\\cmidrule(lr){%i-%i}", min_vals, max_vals))
+      line <- paste(line[line != ""], collapse = " ")
+      
+      header <- paste0(header, "  ", line, "\n  ")
+   }
+   
+   addtorow <- list(pos = list(-1, -1, nrow(df)),
+                    command = c("\\hline\n  ", header, "\\hline\n  "))
+   return(addtorow)
+}
 
 #bias and sd for uninformative sampling (NML, PL, WPL)
 df<- matrix(round(c(apply(Fit_NML, 2,  mean),apply(Fit_NML, 2, sd) , apply(Fit_PL, 2, mean),apply(Fit_PL, 2, sd), apply(Fit_WPL, 2, mean),apply(Fit_WPL, 2, sd)),2),
